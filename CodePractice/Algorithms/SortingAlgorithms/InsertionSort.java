3454dda57313367af34c94760ab4fbd57a58814d
rrsharma7 <85517339+rrsharma7@users.noreply.github.com>
2021-09-01 11:14:28 +0530

    Update InsertionSort.java

--- CodePractice/Algorithms/SortingAlgorithms/InsertionSort.java
+++ CodePractice/Algorithms/SortingAlgorithms/InsertionSort.java
@@ -1,12 +1,23 @@
+/**
+   Time Complexity: O(n^2) 
+   Auxiliary Space: O(1)
+   Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. 
+   And it takes minimum time (Order of n) when elements are already sorted.
+   Algorithmic Paradigm: Incremental Approach
+   Sorting In Place: Yes
+*/
 package sorting;
-
-class InsertionSort {
-	void sort(int arr[]) {
+class InsertionSort 
+{
+	void sort(int arr[]) 
+	{
 		int n = arr.length;
-		for (int i = 1; i < n; ++i) {
+		for (int i = 1; i < n; ++i) 
+		{
 			int key = arr[i];
 			int j = i - 1;
-			while (j >= 0 && arr[j] > key) {
+			while (j >= 0 && arr[j] > key) 
+			{
 				arr[j + 1] = arr[j];
 				j = j - 1;
 			}
@@ -18,16 +29,13 @@ class InsertionSort {
 		int n = arr.length;
 		for (int i = 0; i < n; ++i)
 			System.out.print(arr[i] + " ");
-
 		System.out.println();
 	}
 
 	public static void main(String args[]) {
 		int arr[] = { 12, 11, 13, 5, 6 };
-
 		InsertionSort ob = new InsertionSort();
 		ob.sort(arr);
-
 		printArray(arr);
 	}
 }
