204c7e6de53ce9670c7aab51b1158cbd9032a0c0
rrsharma7 <85517339+rrsharma7@users.noreply.github.com>
2021-10-10 12:45:50 +0530

    Update MergeSort.java

--- CodePractice/Algorithms/SortingAlgorithms/MergeSort.java
+++ CodePractice/Algorithms/SortingAlgorithms/MergeSort.java
@@ -31,44 +31,25 @@ package sorting;
 class MergeSort {
 void merge(int arr[], int l, int m, int r) 
 {
-	int n1 = m - l + 1;
-	int n2 = r - m;
-	int L[] = new int[n1];
-	int R[] = new int[n2];
-	for (int i = 0; i < n1; ++i)
-	{
-		L[i] = arr[l + i];
-	}
-	for (int j = 0; j < n2; ++j)
-	{
-		R[j] = arr[m + 1 + j];
-	}
+	int[] left = Arrays.copyOfRange(arr, l, m + 1);
+	int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
 	int i = 0, j = 0;
 	int k = l;
-	while (i < n1 && j < n2) 
-	{
-		if (L[i] <= R[j]) 
-		{
-			arr[k] = L[i];
+	while (i < left.length && j < right.length) {
+		if (left[i] <= right[j]) {
+			arr[k] = left[i];
 			i++;
-		} else 
-		{
-			arr[k] = R[j];
+		} else {
+			arr[k] = right[j];
 			j++;
 		}
 		k++;
 	}
-	while (i < n1) 
-	{
-		arr[k] = L[i];
-		i++;
-		k++;
+	while (i < left.length) {
+		arr[k++] = left[i++];
 	}
-	while (j < n2) 
-	{
-		arr[k] = R[j];
-		j++;
-		k++;
+	while (j < right.length) {
+		arr[k++] = right[j++];
 	}
 }
 void sort(int arr[], int l, int r) 
