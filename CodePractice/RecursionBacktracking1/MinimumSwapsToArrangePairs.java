0cb3ab47ca7f15a45840a6bed9838d65c0fce38e
rrsharma7 <85517339+rrsharma7@users.noreply.github.com>
2021-10-11 13:29:57 +0530

    Update MinimumSwapsToArrangePairs.java

--- CodePractice/RecursionBacktracking1/MinimumSwapsToArrangePairs.java
+++ CodePractice/RecursionBacktracking1/MinimumSwapsToArrangePairs.java
@@ -1,3 +1,73 @@
+/**
+Minimum Swaps To Arrange Pairs
+Problem Description
+
+There are A pairs and therefore 2A people. Each person has a unique number ranging from 1 to 2A. An array of integers B shows the arrangement of these 2A people.
+
+A matrix C of size A x 2 is given describing pairs i.e. people that are partner of each other. C[i][0] and C[i][1] are partner of each other.
+
+Find the minimum number of swaps required to arrange these pairs such that all pairs become adjacent to each other.
+
+
+
+Problem Constraints
+1 <= A <= 20
+
+1 <= B[i] <= 2*A
+
+C[i][0]!=C[i][1]
+
+1 <= C[i][0], C[i][1] <= 2*A
+
+
+
+Input Format
+The First argument given is an integer A.
+
+The Second argument given is the integer array B.
+
+The Third argument given is matrix C.
+
+
+
+Output Format
+Return the minimum number of swaps required to arrange these pairs such that all pairs become adjacent to each other.
+
+
+
+Example Input
+Input 1:
+
+A = 3
+B = [3, 5, 6, 4, 1, 2]
+C = [   [1, 3]
+        [2, 6]
+        [4, 5]  ]
+Input 2:
+
+A = 1
+B = [1, 2]
+C = [ [1, 2] ]
+
+
+Example Output
+Output 1:
+
+ 2
+Output 2:
+
+ 0
+
+
+Example Explanation
+Explanation 1:
+
+One of the ways to arraange them 
+1. swap(5 and 6) order becomes : [3, 6, 5, 4, 1, 2]
+2. swap(6 and 1) order becomes:  [3, 1, 5, 4, 6, 2]
+Explanation 2:
+
+No swaps required. */
 package RecursionBacktracking1;
 
 import java.util.ArrayList;
