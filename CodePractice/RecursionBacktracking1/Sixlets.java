e2508b580a3cf2bc0847b631d655fda374dddd78
rrsharma7 <85517339+rrsharma7@users.noreply.github.com>
2021-10-11 13:26:52 +0530

    Update Sixlets.java

--- CodePractice/RecursionBacktracking1/Sixlets.java
+++ CodePractice/RecursionBacktracking1/Sixlets.java
@@ -1,3 +1,62 @@
+/**
+SIXLETS
+Problem Description
+
+Given a array of integers A of size N and an integer B.
+
+Return number of non-empty subsequences of A of size B having sum <= 1000.
+
+
+
+Problem Constraints
+1 <= N <= 20
+
+1 <= A[i] <= 1000
+
+1 <= B <= N
+
+
+
+Input Format
+The first argument given is the integer array A.
+
+The second argument given is the integer B.
+
+
+
+Output Format
+Return number of subsequences of A of size B having sum <= 1000.
+
+
+
+Example Input
+Input 1:
+
+    A = [1, 2, 8]
+    B = 2
+Input 2:
+
+    A = [5, 17, 1000, 11]
+    B = 4
+
+
+Example Output
+Output 1:
+
+3
+Output 2:
+
+0
+
+
+Example Explanation
+Explanation 1:
+
+ {1, 2}, {1, 8}, {2, 8}
+Explanation 1:
+
+ No valid subsequence
+ */
 package RecursionBacktracking1;
 
 class Sixlets {
